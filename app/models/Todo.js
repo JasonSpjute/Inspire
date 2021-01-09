3080162f3ffd6647cd31135497493b3bd6034c6f
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-08 17:01:57 -0700

    fixed template error

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -9,13 +9,13 @@ export default class Todo {
     return /*html*/ `
         <div class="row">
             <div class="col-1 text-center">
-              <input id="${this.id}" type="checkbox" onchange="app.TodoController.toggleTodoStatus('${this.id}')">
+              <input id="${this.id}" type="checkbox" onchange="app.todoController.toggleTodoStatus('${this.id}')">
               </div>
             <div class="col-9">
                 <p>${this.description}</p>
             </div>
             <div class="col-1">
-                <button class="fas fa-trash-alt btn text-danger" onclick="app.itemController.delete('${this.id}')"></button>
+                <button class="fas fa-trash-alt btn text-danger" onclick="app.itemController.removeTodo('${this.id}')"></button>
             </div>
         </div>
         `;
