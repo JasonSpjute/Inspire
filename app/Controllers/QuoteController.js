b173326ad815e81c3a29a7989288caa5899f8a06
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-09 21:11:13 -0700

    Quote shows up

--- app/Controllers/QuoteController.js
+++ app/Controllers/QuoteController.js
@@ -1,3 +1,24 @@
+import { ProxyState } from "../AppState.js";
+import quoteService from "../Services/QuoteService.js";
 
-//TODO Create methods for constructor, and rendering the quote to the page
-export default class QuoteController { }
+function _drawQuote(){
+    let quote = ProxyState.quote.body
+    console.log(quote)
+    document.getElementById('quote').innerHTML = quote
+}
+export default class QuoteController{
+
+    constructor(){
+        ProxyState.on("quote", _drawQuote)
+        this.getQuote()
+    }
+
+    getQuote(){
+        try{
+            quoteService.getQuote()
+        }
+        catch(error){
+            console.error(error)
+        }
+    }
+}
--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 import ImageController from "./Controllers/ImageController.js";
+import QuoteController from "./Controllers/QuoteController.js";
 import TodoController from "./Controllers/TodoController.js";
 import WeatherController from "./Controllers/WeatherController.js";
 
 
-//TODO Dont forget to register all your controllers	
 class App {
     weatherController = new WeatherController();
     todoController = new TodoController();
